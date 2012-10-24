d80d8dfc6441e9217d8515ea1863c8a7c0d95267
Jordi Llonch <jordi.llonch@ofertix.com>
2012-10-24 23:32:23 +0200

    wip web front

--- apps/catacomb_core/www/js/catacomb.js
+++ apps/catacomb_core/www/js/catacomb.js
@@ -1,63 +1,89 @@
-$(document).ready(function() {
+$(document).ready(function()
+{
     setStatusNotConnected();
 
+    // controls
     $("#sendButton").click(sendMessage);
     $("#connectButton").click(connect);
     $("#disconnectButton").click(disconnect);
     $("#loginButton").click(login);
     $("#getCharacterListButton").click(getCharacterList);
+
+    // player
+    $("#roomDirNW").click("nw", go);
+    $("#roomDirN").click("n", go);
+    $("#roomDirNE").click("ne", go);
+    $("#roomDirW").click("w", go);
+    $("#roomDirE").click("e", go);
+    $("#roomDirSW").click("sw", go);
+    $("#roomDirS").click("s", go);
+    $("#roomDirSE").click("se", go);
 });
 
-function writeStatus(message) {
+function writeStatus(message)
+{
     var html = document.createElement("div");
     html.setAttribute("class", "message");
     html.innerHTML = message;
-    document.getElementById("status").appendChild(html);
+    document.getElementById("console").appendChild(html);
 }
 
 // Websockets
-function connect() {
+function connect()
+{
     ws = new WebSocket("ws://localhost:8081/ws.yaws");
 
-    ws.onopen = function (evt) {
+    ws.onopen = function (evt)
+    {
         writeStatus("connected");
         setStatusConnected();
     }
 
-    ws.onclose = function (evt) {
+    ws.onclose = function (evt)
+    {
         writeStatus("disconnected");
         setStatusNotConnected();
     }
 
-    ws.onmessage = function (evt) {
+    ws.onmessage = function (evt)
+    {
+        // Write to console
         writeStatus("response: " + evt.data);
+
+        // Process message
+        processResponse(evt.data);
     }
 
-    ws.onerror = function (evt) {
+    ws.onerror = function (evt)
+    {
         writeStatus("error: " + evt.data);
         setStatusNotConnected();
     }
 }
 
-function disconnect() {
+function disconnect()
+{
     ws.close();
 
     setStatusNotConnected();
 }
 
-function sendMessage() {
+function sendMessage()
+{
     var msg = $("#messageField").val();
     ws.send(msg);
 }
 
-function login() {
+function login()
+{
     var user = $("#login").val();
     var password = $("#password").val();
     ws.send('{"type":"login_request","body":{"user":"' + user + '","password":"' + password + '"}}');
     setStatusAuthenticated();
 }
 
-function getCharacterList() {
+function getCharacterList()
+{
     ws.send('{"type":"get_character_list_request","body":"none"}');
 }
 
@@ -107,6 +133,46 @@ function setUI()
             $("#loginButton").attr('disabled', 'disabled');
             $("#getCharacterListButton").attr('disabled', 'disabled');
             $("#sendButton").attr('disabled', 'disabled');
+            $("#roomDirNW").attr('disabled', 'disabled');
+            $("#roomDirN").attr('disabled', 'disabled');
+            $("#roomDirNE").attr('disabled', 'disabled');
+            $("#roomDirW").attr('disabled', 'disabled');
+            $("#roomDirE").attr('disabled', 'disabled');
+            $("#roomDirSW").attr('disabled', 'disabled');
+            $("#roomDirS").attr('disabled', 'disabled');
+            $("#roomDirSE").attr('disabled', 'disabled');
             break;
     }
+}
+
+function processResponse(data)
+{
+    var obj = $.parseJSON(data);
+    console.log(obj);
+    if(obj == null) return;
+
+    switch (obj.type)
+    {
+        case "login_response":
+            break;
+        case "get_character_list_response":
+            break;
+        case "load_character_response":
+            break;
+        case "":
+            break;
+        case "":
+            break;
+        case "":
+            break;
+        case "":
+            break;
+    }
+
+}
+
+function go(direction)
+{
+    console.log('go: ' + direction.data);
+    console.log(direction);
 }
