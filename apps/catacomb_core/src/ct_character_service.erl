021b52ad5dec76eeeea752026bcafa3fd5216384
dcodix <dcodix@gmail.com>
2012-11-21 21:17:17 +0100

    WIP_createUser

--- apps/catacomb_core/src/ct_character_service.erl
+++ apps/catacomb_core/src/ct_character_service.erl
@@ -1,7 +1,7 @@
 -module(ct_character_service).
 -behaviour(gen_server).
 -export([start_link/0,stop/0]).
--export([get_character_list/1,get_character_data/2]).
+-export([get_character_list/1,get_character_data/2,create_character/2,delete_character/2]).
 -export([init/1, handle_call/3,handle_cast/2,terminate/2,code_change/3,handle_info/2]).
 
 -include ("ct_character_info.hrl").
@@ -24,7 +24,10 @@ get_character_list(UserId) ->
     gen_server:call({global,?MODULE}, {get_character_list, UserId}).
 get_character_data(UserId,CharacterId) ->
     gen_server:call({global,?MODULE}, {get_character_data, UserId, CharacterId}).
-    
+create_character(UserId,Name) ->
+    gen_server:call({global,?MODULE}, {create_character, UserId, Name}).
+delete_character(UserId,PublicId) ->
+    gen_server:call({global,?MODULE}, {delete_character, UserId, PublicId}).
 
 %% User Callbacks
 handle_call({get_character_list, UserId}, _From, State) ->
@@ -35,15 +38,15 @@ handle_call({get_character_list, UserId}, _From, State) ->
     Result = emysql:execute(ct_auth_pool, "SELECT name,public_id,max_life_points,life_points,level FROM `character` WHERE user_id=" ++ emysql_util:encode(UserId)),
 	case Result of
 		#result_packet{rows=[]} ->
-			{reply, {ok, []}, State};
+			{reply, {ok_packet, []}, State};
 		#result_packet{} ->
 		    List = emysql_util:as_proplists(Result,fun(A)-> {obj,A} end),
-			{reply, {ok, List}, State};
+			{reply, {ok_packet, List}, State};
 		#ok_packet{} ->
-			{reply, {error, sql_error}, State};
+			{reply, {error_packet, sql_error}, State};
 		#error_packet{} ->
-			{reply, {error, sql_error}, State};
-		_ -> {reply, {error, sql_error}, State}
+			{reply, {error_packet, sql_error}, State};
+		_ -> {reply, {error_packet, sql_error}, State}
 	end;	
 handle_call({get_character_data,UserId,PublicCharacterId}, _From, State) ->
 	%Get character data from DB
@@ -57,6 +60,39 @@ handle_call({get_character_data,UserId,PublicCharacterId}, _From, State) ->
 			{reply, {ok, List}, State};
 		#ok_packet{} ->
 			{reply, {error, sql_error}, State};
+		#error_packet{} ->
+			{reply, {error, sql_error}, State};
+		_ -> {reply, {error, sql_error}, State}
+	end;
+handle_call({create_character,UserId,Name}, _From, State) ->
+	MaxLifePoints=1000,
+	LifePoints=1000,
+	HitChance=30,
+	DodgeChance=10,
+	MaxDamage=50,
+	MinDamage=1,
+	Armor=1,
+	Level=1,
+	ExperiencePoints=1,
+
+	%Insert to db
+	SQL=io_lib:format("INSERT INTO `character`(user_id,name,max_life_points,life_points,hit_chance,dodge_chance,max_damage,min_damage,armor,level,experience_points,coord_x,coord_y,public_id) VALUES (~s,~s,~s,~s,~s,~s,~s,~s,~s,~s,~s,~s,~s,~s)",[emysql_util:encode(UserId),emysql_util:encode(Name),emysql_util:encode(MaxLifePoints),emysql_util:encode(LifePoints),emysql_util:encode(HitChance),emysql_util:encode(DodgeChance),emysql_util:encode(MaxDamage),emysql_util:encode(MinDamage),emysql_util:encode(Armor),emysql_util:encode(Level),emysql_util:encode(ExperiencePoints),emysql_util:encode(5),emysql_util:encode(5),emysql_util:encode(9999)]),
+	Result = emysql:execute(ct_auth_pool, SQL ),
+	case Result of
+		#ok_packet{} ->
+			{reply, {ok, []}, State};
+		#error_packet{} ->
+			{reply, {error, sql_error}, State};
+		_ -> {reply, {error, sql_error}, State}
+	end;
+handle_call({delete_character,UserId,PublicId}, _From, State) ->
+
+	%Insert to db
+	SQL=io_lib:format("DELETE FROM `character` WHERE user_id=~s AND public_id=~s",[emysql_util:encode(UserId),emysql_util:encode(PublicId)]),
+	Result = emysql:execute(ct_auth_pool, SQL ),
+	case Result of
+		#ok_packet{} ->
+			{reply, {ok, []}, State};
 		#error_packet{} ->
 			{reply, {error, sql_error}, State};
 		_ -> {reply, {error, sql_error}, State}
